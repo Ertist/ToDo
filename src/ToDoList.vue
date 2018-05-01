fb3cb45d80d213d6bf7c89610dac084e93084f80
kym <ofxiaoka@163.com>
2018-05-01 15:01:09 +0800

    todo-list vue-cli

---
+++ src/ToDoList.vue
@@ -0,0 +1,38 @@
+<template>
+ <div>
+   <h1>todolist</h1>
+   <input type="text" v-model="listMsg" />
+   <button @click = "addList">add</button>
+   <ul>
+       <todo-item v-for="(item, index) of list" :key="index" :content="item" :index="index" @itemDelete="deleteList"></todo-item>
+   </ul>
+ </div>
+</template>
+
+<script>
+import listItem from './components/listItem.vue'
+export default {
+  components: {
+    'todo-item': listItem
+  },
+  data () {
+    return {
+      listMsg: '',
+      list: []
+    }
+  },
+  methods: {
+    addList: function () {
+      this.list.push(this.listMsg)
+      this.listMsg = ''
+    },
+    deleteList: function (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/listItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div @click = "handleDelete">{{content}}</div>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleDelete: function (index) {
+      this.$emit('itemDelete', index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
